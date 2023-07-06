1b47c88860d6aee95f0bc632ddbe7189efdda6cf
jacquesdouglasdev <jacquesdouglasdev@gmail.com>
2023-07-06 16:29:39 -0300

    projeto configurado para inicio das aulas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  safelist: [
+    { pattern: /^gap-/ },
+    { pattern: /^w-/ },
+    { pattern: /^h-/ },
+    { pattern: /^text-/ },
+    { pattern: /^bg-/ },
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [require('tailwind-scrollbar')],
 }
